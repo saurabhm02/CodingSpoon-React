628120bda6347c3285890a23419e37dc300b4224
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2024-01-15 18:46:50 +0530

    update file

--- Day - 01[12Jan]/index.js
+++ Day - 01[12Jan]/index.js
@@ -1,69 +1,69 @@
 
-// obj1 = {
-//     firstName: 'John',
-//     lastName: 'Doe',
-//     address: '123 Main',
-// }
+obj1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    address: '123 Main',
+}
 
-// const {firstName, lastName, address} = obj1;
-// console.log(firstName, lastName, address);
+const {firstName, lastName, address} = obj1;
+console.log(firstName, lastName, address);
 
-// const arr = [
-//     {
-//         name: 'John',
-//         address: '123 Main',
-//     },
-//     {
-//         name: 'Doe',
-//         address: '126 Main',
-//     },
-// ];
+const arr = [
+    {
+        name: 'John',
+        address: '123 Main',
+    },
+    {
+        name: 'Doe',
+        address: '126 Main',
+    },
+];
 
-// const [ ob1, ob2 ] = arr;
-// const { fName, lName } = ob1;
-// console.log(fName, lName);
-
-
-// const alphaObj = {
-//     firstName: ['a', 'b', 'c'],
-//     lastName: ['d', 'e', 'f'],
-// }
+const [ ob1, ob2 ] = arr;
+const { fName, lName } = ob1;
+console.log(fName, lName);
 
 
-// const { firstName: fNames, lastName: lNames } = alphaObj;
-// const [a1, a2, a3] = fNames;
-// const [a4, a5, a6] = lNames;
-// console.log(a1, a2, a3);
-// console.log(a4, a5, a6);
-
-// alphaObj.firstName.map((e) => {
-//     return(console.log(e));
-// })
-// alphaObj.lastName.map((e) => {
-//     return(console.log(e));
-// });
+const alphaObj = {
+    firstName: ['a', 'b', 'c'],
+    lastName: ['d', 'e', 'f'],
+}
 
 
+const { firstName: fNames, lastName: lNames } = alphaObj;
+const [a1, a2, a3] = fNames;
+const [a4, a5, a6] = lNames;
+console.log(a1, a2, a3);
+console.log(a4, a5, a6);
 
-// obj1 = {
-//     firstName: 'John',
-//     lastName: 'Doe',
-//     address: {
+alphaObj.firstName.map((e) => {
+    return(console.log(e));
+})
+alphaObj.lastName.map((e) => {
+    return(console.log(e));
+});
 
-//         city: 'New York',
-//         state: 'NY',
-//     },
-// }
 
-// const bObj = obj1;
-// bObj['firstName'] = 'abhi';
-// console.log(bObj);
-// console.log(obj1);
 
-// const obj3 = {...obj1};
-// obj3['address']['city'] = 22;
-// console.log(obj3);
-// console.log(obj1);
+obj1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    address: {
+
+        city: 'New York',
+        state: 'NY',
+    },
+}
+
+const bObj = obj1;
+bObj['firstName'] = 'abhi';
+console.log(bObj);
+console.log(obj1);
+
+const obj3 = {...obj1};
+obj3['address']['city'] = 22;
+console.log(obj3);
+console.log(obj1);
 
 const add = require('./add');
 
--- Day - 02[13Jan]/index.js.js
+++ Day - 02[13Jan]/index.js.js
@@ -1,25 +1,25 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// fs.readFile('./data.js', {encoding: 'utf-8'}, (err, data) => {
-//     if(err) {
-//         console.log(err);
-//     } else {
-//         console.log(data);
-//     }
-// });
+fs.readFile('./data.js', {encoding: 'utf-8'}, (err, data) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
 
-// const promise = new Promise((res, rej)=> {
-//     res("Hi data is here");
-// });
+const promise = new Promise((res, rej)=> {
+    res("Hi data is here");
+});
 
-// console.log(promise);
-// promise.then((data) =>{
-//     console.log(data);
-// }).catch((err) =>{
-//     console.log(err);
-// }).finally(() =>{
-//     console.log("I am finally");
-// })
+console.log(promise);
+promise.then((data) =>{
+    console.log(data);
+}).catch((err) =>{
+    console.log(err);
+}).finally(() =>{
+    console.log("I am finally");
+})
 
 
 const getData = async(a, b) =>{
